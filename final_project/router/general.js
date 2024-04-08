48ac089cf6c5b4cad4aeecead7e73799badb65a5
Belal Arar <barar@live.com>
2024-04-08 17:31:36 +0300

    change to promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,25 +20,64 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  return res.status(200).json(JSON.stringify(books));
+  fetchBooks() 
+    .then(books => {
+      res.status(200).json(books);
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  var filteredBooks = books[req.params.isbn];
-  return res.status(200).json(JSON.stringify(filteredBooks));
+  fetchBookByISBN(req.params.isbn)
+    .then(book => {
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ error: 'Book not found' });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching book:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  var filteredBooks = Object.values(books).filter(book => book.author === req.params.author);;
-  return res.status(200).json(JSON.stringify(filteredBooks));
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      var filteredBooks = Object.values(books).filter(book => book.author === req.params.author);
+      resolve(filteredBooks);
+    }, 1000);
+  })
+  .then(filteredBooks => {
+    res.status(200).json(filteredBooks);
+  })
+  .catch(error => {
+    console.error('Error fetching books by author:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  var filteredBooks = Object.values(books).filter(book => book.title === req.params.title);
-  return res.status(200).json(JSON.stringify(filteredBooks));
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      var filteredBooks = Object.values(books).filter(book => book.title === req.params.title);
+      resolve(filteredBooks);
+    }, 1000);
+  })
+  .then(filteredBooks => {
+    res.status(200).json(filteredBooks);
+  })
+  .catch(error => {
+    console.error('Error fetching books by author:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  });
 });
 
 //  Get book review
@@ -46,5 +85,24 @@ public_users.get('/review/:isbn', function (req, res) {
   var myBook = books[req.params.isbn].reviews;
   return res.status(200).json(JSON.stringify(myBook));
 });
+
+function fetchBooks() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books);
+
+      reject(new Error("Failed to fetch books"));
+    }, 1000);
+  });
+}
+
+function fetchBookByISBN(isbn) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books[isbn]);
+      reject(new Error("Failed to fetch book by ISBN"));
+    }, 1000);
+  });
+}
 
 module.exports.general = public_users;
